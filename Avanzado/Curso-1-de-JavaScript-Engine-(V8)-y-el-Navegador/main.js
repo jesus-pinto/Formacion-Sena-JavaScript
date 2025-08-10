92d0eea39435ac15a633a3d9cdf81f783ea1d08f
jesuspinto <jesus.pinto@itglobers.com>
2025-08-10 13:56:24 -0400

    Js

---
+++ Avanzado/Curso-1-de-JavaScript-Engine-(V8)-y-el-Navegador/main.js
@@ -0,0 +1,19 @@
+const carro = {
+    marca: "Toyota",
+    modelo: "2025"
+}
+
+console.log(`Yo soy Jesus Pinto y tengo un carro, marca ${carro.marca} modelo ${carro.modelo}`);
+
+function restarDos(num){
+    return num-2;
+}
+function calcular(){
+    const sumartotal = 4+5;
+    return restarDos(sumartotal);
+}
+
+debugger;
+
+calcular();
+
